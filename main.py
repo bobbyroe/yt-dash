45db20dc2ee24b52c2dcad6aba79af64ab73bf90
Bobby Roe <bobby.roe@unifyconsulting.com>
2025-01-07 14:34:45 -0800

    set theme, add date range select

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv('./yt-data.csv')
+st.title("🤖 Robot Bobby YouTube Dashboard")
+st.write("Welcome! Here are the latest stats for the Robot Bobby YouTube channel.")
+date_options_map = {
+    "Week": "week.csv",
+    "Month": "month.csv",
+    "Quarter": "quarter.csv",
+    "Year": "year.csv",
+    "All Time": "all.csv"
+}
+date_range = st.selectbox("Select date", list(date_options_map.keys()))
 
+data = pd.read_csv(f'./data/{date_options_map[date_range]}')
 totals = data[data['Content'] == 'Total'] # get totals row
 
 data = data.drop(columns=['Content'])
@@ -11,7 +21,7 @@ data['Watch time (hours)'] = data['Watch time (hours)'].round(0) # round hours v
 data['Duration'] = (data['Duration'] / 60).round(1) # convert to minutes
 data = data.iloc[:, [1, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]] # integer location based indexing
 
-st.title("🤖 Robot Bobby YouTube Dashboard")
+
 colL, colM, colR = st.columns(3)
 colL.metric("Total Views", totals['Views'])
 colM.metric("Subscribers", totals['Subscribers'])
@@ -20,7 +30,7 @@ colR.metric("Videos", data.shape[0])
 st.subheader("Data Preview")
 st.write(data.head())
 
-threshold = 50
+threshold = 5
 filtered_data = data[data['Watch time (hours)'] > threshold]
 columns = ['Video title', 'Comments added', 'Shares', 'Likes',] #'Watch time (hours)', ] # 'Average view duration']
 filtered_data = pd.DataFrame(filtered_data, columns=columns)
